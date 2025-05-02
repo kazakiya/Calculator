cb6ca463a7c2ba19ca7bdaf4587c9bc3ab79113e
Chainarong Sangsawang <chainarong6@hotmail.com>
2025-05-02 10:50:40 +0700

    add gui calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+
+GUI = Tk()
+GUI.title('Basic Git & GitHub By Kak')
+GUI.geometry('500x500')
+
+L = Label(GUI,text='Chainarong Sangsawang')
+L.pack()
+
+L = Label(GUI,text='Jirarat')
+L.pack()
+
+GUI.mainloop()
